061421bfeee089e62d32c9a210cc22922d87025a
Bonheur ANAMLI BUSHIRI <Bonheur Bushiri>
2022-06-02 23:07:04 +0200

    add_form_scriptjs

--- form/script.js
+++ form/script.js
@@ -1,4 +1,13 @@
 /*  ================ */
 /*  PAGE FORMULAIRE */
 /*  =============== */
+const nomImput = document.querySelector("name");
+const email = document.querySelector("email");
+const message = document.querySelector("message");
+const succes = document.querySelector("succes");
+const errorNodes = document.querySelector("error");
 
+
+function validateForm(){
+
+}
