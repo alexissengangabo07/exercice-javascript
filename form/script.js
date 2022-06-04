9cd224aef9d36f3ac4fda63e167fb3dbfacab5d6
Bonheur ANAMLI BUSHIRI <Bonheur Bushiri>
2022-06-04 12:44:26 +0200

    button_script1

--- form/script.js
+++ form/script.js
@@ -12,26 +12,41 @@ const errorNodes = document.querySelector("error");
 function validateForm(){
 
 clearMessage();
+let errorFlag = false;
 
-if(nameInput.value.length <1){
+if(nameInput.value.length < 1){
     errorNodes[0].innerText = "Name Cannot be blanK";
     nameInput.classList.add("error-border");
+    errorFlag = true;
   }
 
   if(!emailIsValid(email.value)){
-    errorNodes[0].innerText = "Invalid email adress";
+    errorNodes[1].innerText = "Invalid email adress";
     email.classList.add("error-border");
+    errorFlag = true;
   }
-
-
+   if (message.value.length < 1){
+    errorNodes[2].innerText = "Tappez le message svp";
+    message.classList.add("error-border");
+    errorFlag = true;
+}
+    if(!errorFlag){
+        succes.innerText = "Success!";
+    }
 }
 
 //efface l'erreur/message succès 
 function clearMessage(){
     for (let i = 0; i < errorNodes.length; i++){
-        errorNodes.classList.remove("error-border");
+        errorNodes[i].innerText= "";
     }
-}// tester la validité du mail
+    succes.innerText = "";
+    nameInput.classList.remove("error-border");
+    email.classList.remove("error-border");
+    message.classList.remove("error-border");
+}
+
+// tester la validité du mail
  function emailIsValid(email){
      let pattern = /\S+@\S+\.\S+/;
      return pattern.test(email)
