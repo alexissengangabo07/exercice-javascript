3cf499b4d7caf0aa7e34b3d8646a4b9f50d7d6d6
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-06-06 16:13:37 +0200

    feat: add delete-button and modify-button

--- todolist/script.js
+++ todolist/script.js
@@ -8,5 +8,22 @@ button.addEventListener("click", addTodo);
 function addTodo(event) {
     event.preventDefault();
     //Todo Div
+    const tododiv = document.createElement("div");
+    tododiv.classList.add("todo")
+        // CREATION DE LA LIST
+    const list = document.createElement("list_");
+    list.classList.add("todo_list");
+    // INSERTION DE LA LIST DANS LA DIV
+    tododiv.appendChild(list);
+    // CREATION DES DEUX BUTTON (MODIFIER) & (SUPPRIMER)  
+    const todobutton = document.createElement("btn");
+    todobutton.classList.add("btn_del");
+    todobutton.innerHTML = '<i class="fa fa-check " > </i>';
+    tododiv.appendChild(todobutton);
+    const todobutton2 = document.createElement("btn");
+    todobutton2.classList.add("btn_trash");
+    todobutton2.innerHTML = '<i class="fa fa-trash " > </i>';
+    tododiv.appendChild(todobutton2);
+    todolist.appendChild(tododiv);
 
 }
